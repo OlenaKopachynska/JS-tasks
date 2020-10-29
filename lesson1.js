c0e4f340c92e5818cbd067a9f1d508dff592305e
3048 <olena99y@gmail.com>
2020-10-29 23:20:45 +0200

    part one done

---
+++ lesson1.js
@@ -0,0 +1,282 @@
+// ---------------------------Task 1--------------------------
+
+// let welcome = 'hello';
+// let academy = 'owu';
+// let domainNAme = 'com';
+// let countryOfOrigin = 'ua';
+// let firsrNumber = 1;
+// let years = 10;
+// let negativeNum = -999;
+// let randomNum = 123;
+// let piNum = 3.14;
+// let decimalNum = 2.7;
+// let numOfProjects = 16;
+// let x = true;
+// let y = false;
+
+// console.log(welcome);
+// console.log(academy);
+// console.log(domainNAme);
+// console.log(countryOfOrigin);
+// console.log(firsrNumber);
+// console.log(years);
+// console.log(negativeNum);
+// console.log(randomNum);
+// console.log(piNum);
+// console.log(decimalNum);
+// console.log(numOfProjects);
+// console.log(x);
+// console.log(y);
+
+// alert(welcome);
+// alert(academy);
+// alert(domainNAme);
+// alert(countryOfOrigin);
+// alert(firsrNumber);
+// alert(years);
+// alert(negativeNum);
+// alert(randomNum);
+// alert(piNum);
+// alert(decimalNum);
+// alert(numOfProjects);
+// alert(x);
+// alert(y);
+
+//document.write(welcome + " " + academy + " " + domainNAme + " " + countryOfOrigin + " " + firsrNumber + " " + years + " " + negativeNum + " " + randomNum + " " + piNum + " " + decimalNum + " " + numOfProjects + " " + x + " " + y);
+
+
+// -----------------------------Task 2 ------------------------
+
+// welcome = "Nice to meet you";
+// academy = "JAVA courses";
+// domainNAme = "net"
+// countryOfOrigin = "gb"
+// firsrNumber = 0;
+// years = 12;
+// negativeNum = -555;
+// randomNum = 234;
+// piNum = 3.15;
+// decimalNum = 50.567;
+// numOfProjects = 2;
+// x = 3;
+// y = 4;
+
+// console.log(welcome);
+// console.log(academy);
+// console.log(domainNAme);
+// console.log(countryOfOrigin);
+// console.log(firsrNumber);
+// console.log(years);
+// console.log(negativeNum);
+// console.log(randomNum);
+// console.log(piNum);
+// console.log(decimalNum);
+// console.log(numOfProjects);
+// console.log(x);
+// console.log(y);
+
+// alert(welcome);
+// alert(academy);
+// alert(domainNAme);
+// alert(countryOfOrigin);
+// alert(firsrNumber);
+// alert(years);
+// alert(negativeNum);
+// alert(randomNum);
+// alert(piNum);
+// alert(decimalNum);
+// alert(numOfProjects);
+// alert(x);
+// alert(y);
+
+//document.write(welcome + " " + academy + " " + domainNAme + " " + countryOfOrigin + " " + firsrNumber + " " + years + " " + negativeNum + " " + randomNum + " " + piNum + " " + decimalNum + " " + numOfProjects + " " + x + " " + y);
+
+
+// ----------------------- Task 3 ------------------------
+
+// const name1 = "James";
+// const name2 = "Dan";
+// const name3 = "Mark";
+
+
+// console.log(name1);
+// console.log(name2);
+// console.log(name3);
+
+// alert(name1);
+// alert(name2);
+// alert(name3);
+
+// document.write(name1 + ", " + name2 + ", " + name3);
+
+// --------------------- Task 4 -------------------------
+
+// let firstName = prompt('What is your first name?');
+// let lastName = prompt('What is your last name?');
+// let surName = prompt('What is your surname?');
+
+// console.log(firstName);
+// console.log(lastName);
+// console.log(surName);
+
+// alert(firstName);
+// alert(lastName);
+// alert(surName);
+
+// document.write(firstName + " " + lastName + " " + surName);
+
+// ----------------------------- Task 5 -----------------------------
+
+// let person = firstName + " " + lastName + " " + surName;
+// console.log(person);
+
+// ---------------------------- Task 6 -------------------
+
+//let firstName = prompt('What is your first name?');
+// let lastName = prompt('What is your last name?');
+// let surName = prompt('What is your surname?');
+
+// let mother = firstName + " " + lastName + " " + surName;
+// console.log(mother);
+
+// let father = firstName + " " + lastName + " " + surName;
+// console.log(father);
+
+// let elderBrother = firstName + " " + lastName + " " + surName;
+// console.log(elderBrother);
+
+// let youngerBrother = firstName + " " + lastName + " " + surName;
+// console.log(youngerBrother);
+
+// let youngerSister = firstName + " " + lastName + " " + surName;
+// console.log(youngerSister);
+
+// ------------------------------- Task 7 -------------------
+
+// let num1 = prompt('Enter your first number');
+// let num2 = prompt('Enter your second number');
+// let num3 = prompt('Enter your third number');
+
+// console.log(+num1);
+// console.log(+num2);
+// console.log(+num3);
+
+// ------------------------------- Task 8 -------------------
+
+// let number1 = prompt('Enter your first number');
+// let number2 = prompt('Enter your second number');
+// let number3 = prompt('Enter your third number');
+// let number4 = prompt('Enter your forth number');
+
+
+
+// let parsedNumber1 = parseInt(number1);
+// let parsedNumber2 = parseInt(number2);
+// let parsedNumber3 = parseInt(number3);
+// let parsedNumber4 = parseInt(number4);
+
+// let result = parsedNumber1 + parsedNumber2 + parsedNumber3 + parsedNumber4;
+
+// console.log(result);
+
+
+// ------------------------------- Task 9 -------------------
+
+// let number1 = prompt('Enter your first number');
+// let number2 = prompt('Enter your second number');
+// let number3 = prompt('Enter your third number');
+
+// let parsedNumber1 = parseFloat(number1);
+// let parsedNumber2 = parseFloat(number2);
+// let parsedNumber3 = parseFloat(number3);
+
+// let result = parsedNumber1 + parsedNumber2 + parsedNumber3;
+
+// console.log(result);
+
+
+// ------------------------------- Task 10 -------------------
+
+// let number1 = prompt('Enter your first number');
+// let number2 = prompt('Enter your second number');
+// let number3 = prompt('Enter your third number');
+
+// let roundedNumber1 = Math.round(number1);
+// let roundedNumber2 = Math.round(number2);
+// let roundedNumber3 = Math.round(number3);
+
+// let result = roundedNumber1 + roundedNumber2 + roundedNumber3;
+
+// console.log(result);
+
+// ------------------------------- Task 11 -------------------
+
+// let number1 = prompt('Введіть число');
+// let number2 = prompt('Введіть степінь');
+
+// let parsedNumber1 = parseInt(number1);
+// let parsedNumber2 = parseInt(number2);
+
+// let result = Math.pow(parsedNumber1, parsedNumber2);
+
+// console.log(result);
+
+// ------------------------------- Task 11 -------------------
+
+// let a = 100;
+// let b = '100';
+// let c = true;
+// let d = undefined;
+
+// console.log(typeof(a));
+// console.log(typeof(b));
+// console.log(typeof(c));
+// console.log(typeof(d));
+
+// ------------------------------- Task 12 -------------------
+
+
+// let cond1 = 5 < 6; // true
+// let cond2 = 5 >= 6; // false
+// let cond3 = 5 > 6; // false
+// let cond4 = 5 == 6; // false
+// let cond5 = 10 == 10; // true
+// let cond6 = 10 >= 10; // true
+// let cond7 = 10 < 10; // false
+// let cond8 = 10 > 10; // false
+// let cond9 = 10 != 10; // false
+// let cond10 = 123 > 123; // false
+// let cond11 = 123 >= 123; // true
+
+
+/* ******* CLASS ******* */
+    
+    
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
